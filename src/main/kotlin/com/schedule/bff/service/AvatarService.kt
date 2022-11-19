036b8c26080b928d231c5db9f73c9afd28b0d6a6
Vsevolod <seva151201@gmail.com>
2022-11-19 19:18:19 +0700

    Fix avatar getting query params

--- src/main/kotlin/com/schedule/bff/service/AvatarService.kt
+++ src/main/kotlin/com/schedule/bff/service/AvatarService.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
+import org.springframework.web.util.UriComponentsBuilder
 
 interface IAvatarService {
     fun getAvatarsByIds(id: List<Long>): List<Avatar>
@@ -16,8 +17,14 @@ class AvatarService(
     @Qualifier("avatarRestTemplate") private val avatarRestTemplate: RestTemplate
 ) : IAvatarService {
     override fun getAvatarsByIds(id: List<Long>): List<Avatar> {
+        val uri = UriComponentsBuilder
+            .fromHttpUrl("/list")
+            .queryParam("usersIds", id.joinToString(","))
+            .build()
+            .encode()
+            .toUri()
         return avatarRestTemplate
-            .getForEntity<GetAvatarsByIdsResponse>("/list", mapOf("usersIds" to id))
+            .getForEntity<GetAvatarsByIdsResponse>(uri)
             .body!!
             .avatars
     }
