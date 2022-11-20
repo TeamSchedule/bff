fc7184c03bd74289cec5c3bb1c7e51be9568f56d
Vsevolod <seva151201@gmail.com>
2022-11-20 20:43:50 +0700

    Add converters

--- src/main/kotlin/com/schedule/bff/api/model/TeamInviteStatus.kt
+++ src/main/kotlin/com/schedule/bff/api/model/TeamInviteStatus.kt
@@ -4,24 +4,21 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
 import java.util.stream.Stream
 
-enum class TeamInviteStatus(private val code: String) {
+enum class TeamInviteStatus(@get:JsonValue val code: String) {
     ACCEPTED("accepted"),
     REJECTED("rejected"),
     CLOSED("closed"),
     OPEN("open");
 
-    @JsonCreator
-    open fun decode(code: String): TeamInviteStatus? {
-        return Stream.of(*values())
-            .filter { targetEnum: TeamInviteStatus ->
-                targetEnum.code == code.lowercase()
-            }
-            .findFirst()
-            .orElse(null)
-    }
-
-    @JsonValue
-    open fun getCode(): String {
-        return code
+    companion object {
+        @JsonCreator
+        fun decode(code: String): TeamInviteStatus? {
+            return Stream.of(*values())
+                .filter { targetEnum: TeamInviteStatus ->
+                    targetEnum.code == code.lowercase()
+                }
+                .findFirst()
+                .orElse(null)
+        }
     }
 }
